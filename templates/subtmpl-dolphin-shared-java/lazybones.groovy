7548b0ae5d6c7511b86df2ca44d0c670010bf6ab
Sven Ehrke <sven.ehrke@canoo.com>
2014-07-27 12:09:21 +0200

    fixed shared subtemplate

--- templates/subtmpl-dolphin-shared-java/lazybones.groovy
+++ templates/subtmpl-dolphin-shared-java/lazybones.groovy
@@ -11,12 +11,21 @@ params.moduleName = ask2('moduleName', 'shared')
 // Pass in parameters from the project template
 params.parentGroup = parentParams.group
 params.parentVersion = parentParams.version
+params.GROUP = parentParams.group
+params.VERSION = parentParams.version
 
-processTemplates("content/src/main/**/*", params)
+processTemplates("content/**/*", params)
 
 def pkgPath = params.pkg.replace('.' as char, '/' as char)
 
+// Copy 'src' folder:
 FileUtils.moveDirectoryToDirectory(new File(templateDir, 'content/src'), new File(params.moduleName), true )
+
+// Copy files in root folder:
+['build.gradle', 'gradle.properties'].each { fn ->
+	FileUtils.copyFileToDirectory(new File(templateDir, 'content/' + fn), new File(params.moduleName))
+}
+
 
 def ask2(key, proposal) {
 	ask("Define value for '$key' [$proposal]: ", proposal, key)
