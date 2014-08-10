e30b7003520a6ae0524d31ba51eee5a5285de83d
Sven Ehrke <sven.ehrke@canoo.com>
2014-08-10 18:23:04 +0200

    started improvement of content of groovy application

--- templates/subtmpl-dolphin-combined-groovy/content/src/main/groovy/start.groovy
+++ templates/subtmpl-dolphin-combined-groovy/content/src/main/groovy/start.groovy
@@ -2,13 +2,13 @@ package ${PKG}
 
 import org.opendolphin.core.client.comm.JavaFXUiThreadHandler
 import org.opendolphin.core.comm.DefaultInMemoryConfig
-import ${PKG}.MainRegistrarAction
+import ${PKG}.ApplicationDirector
 import ${PKG}.MainView
 
 def config = new DefaultInMemoryConfig()
 config.clientDolphin.clientConnector.uiThreadHandler = new JavaFXUiThreadHandler()
 config.serverDolphin.registerDefaultActions()
 
-config.serverDolphin.register(new MainRegistrarAction())
+config.serverDolphin.register(new ApplicationDirector())
 
 MainView.show(config.clientDolphin)
