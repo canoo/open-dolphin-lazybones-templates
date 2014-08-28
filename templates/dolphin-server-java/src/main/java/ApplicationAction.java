abdfc308a3332f7ad13792666a11b9f93bef9c09
Sven Ehrke <sven.ehrke@canoo.com>
2014-08-28 17:41:08 +0200

    added template 'dolphin-server-java'

---
+++ templates/dolphin-server-java/src/main/java/ApplicationAction.java
@@ -0,0 +1,24 @@
+package ${PKG};
+
+import org.opendolphin.core.comm.Command;
+import org.opendolphin.core.server.action.DolphinServerAction;
+import org.opendolphin.core.server.comm.ActionRegistry;
+import org.opendolphin.core.server.comm.CommandHandler;
+
+import static ${PKG}.ApplicationConstants.*;
+
+import java.util.List;
+
+public class ApplicationAction extends DolphinServerAction{
+
+    public void registerIn(ActionRegistry actionRegistry) {
+
+        actionRegistry.register(ApplicationConstants.COMMAND_GREET, new CommandHandler<Command>() {
+            public void handleCommand(Command command, List<Command> response) {
+                System.out.println("Server reached.");
+				getServerDolphin().getAt(PM_APP).getAt(ATT_GREETING).setValue("Hey " + getServerDolphin().getAt(PM_APP).getAt(ATT_NAME).getValue() + " !");
+            }
+        });
+
+    }
+}
