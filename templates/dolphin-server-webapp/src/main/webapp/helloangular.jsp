26dfb93bcb3d4c132c462452c20310d121745b1d
Sven Ehrke <sven.ehrke@canoo.com>
2014-09-03 14:07:47 +0200

    angular

---
+++ templates/dolphin-server-webapp/src/main/webapp/helloangular.jsp
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+	<script type="text/javascript" src="libs/angularjs/1.2.21/angular.min.js"></script>
+	<script type="text/javascript" src="js/app/main.js"></script>
+
+    <!-- refer to OpenDolphin, see also http://open-dolphin.org/dolphin_website/Download.html -->
+    <script data-main="js/dolphin/" src="libs/require.js"></script>
+
+	<script>
+		var globalDolphin;
+
+		angular.element(document).ready(function() {
+
+			require([ 'opendolphin' ], function (dol) {
+				// Set up the dolphin for client-side only mode (first parameter url is null)
+				// and not forcing a new session on page reload (irrelevant for client side only mode):
+				globalDolphin = dol.dolphin(null, false);
+
+				// Make an attribute with name, no qualifier, and an empty String as initial value:
+				const attribute = globalDolphin.attribute("myAttribute", undefined, "");
+
+				// ... and put it into a presentation model with id 'myPM' and no type
+				var pm = globalDolphin.presentationModel("myPM", undefined, attribute);
+
+				angular.bootstrap(document, ['app']);
+
+			});
+
+		});
+	</script>
+
+
+</head>
+
+<body ng-controller="DemoCtrl">
+
+<input type="text" ng-model="firstName">
+<h1>AngularJS Model: {{firstName}}</h1>
+
+<button ng-click="appendLonger()">add 'longer' to PM</button>
+
+</body>
+</html>
---
+++ templates/dolphin-server-webapp/src/main/webapp/js/app/main.js
@@ -0,0 +1,69 @@
+var app = angular.module('app', []);
+
+app.controller('DemoCtrl', function($scope, $timeout) {
+
+	$scope.init = function()
+	{
+		console.log("controller.init");
+	};
+
+	$scope.firstName = "Sven";
+
+    $scope.appendLonger = function (data) {
+        console.log("hallo " + $scope.firstName);
+		var attr = globalDolphin.getAt('myPM').getAt('myAttribute');
+		attr.setValue(attr.getValue() + ' longer');
+    };
+
+	$scope.bindPM = function(ngModelName, pmId, attrName) {
+		$scope.$watch(ngModelName, function(newVal, oldVal) {
+			console.log("ddd: ", oldVal, " -> ",  newVal);
+
+			if (globalDolphin) {
+				var pm = globalDolphin.getAt(pmId);
+				if (pm) {
+					var attr = globalDolphin.getAt(pmId).getAt(attrName);
+					if (attr) {
+						attr.setValue(newVal);
+					}
+					else {
+						console.log("ERROR: cannot find attribute: ", attrName, " on pm: ", pmId)
+					}
+				}
+				else {
+					console.log("ERROR: cannot find pm: ", pmId)
+				}
+			}
+
+			// Bind value of attribute to ng-model:
+			globalDolphin.getAt(pmId).getAt(attrName).onValueChange(function (event) {
+				console.log("dolphin model changed: ", event);
+				if (event.newValue === null) {
+					console.log("newValue is null");
+					return;
+				}
+				if (event.newValue === event.oldValue) {
+					console.log("values are the same");
+					return;
+				}
+				if (event.newValue === $scope[ngModelName]) {
+					console.log("dolphin and ng-model are the same");
+					return;
+				}
+				$timeout(function() { // prevent nested apply problem. See https://docs.angularjs.org/error/$rootScope/inprog?p0=$apply
+					$scope[ngModelName] = event.newValue;
+				}, 0, true);
+			});
+
+		});
+
+	};
+
+	$scope.bindPM('firstName', 'myPM', 'myAttribute');
+
+});
+
+
+
+
+
